fef9f8214eea279cb08e0f4cc29e7feaf6c5ee68
omelet2409 <thelong2409@gmail.com>
2023-05-07 13:15:23 +0700

    agent testcase

--- home/tests.py
+++ home/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import agents_model
 from django.contrib.auth import authenticate, get_user_model
 
 User = get_user_model()
-
+#admin test login
 class LoginTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(
@@ -21,5 +23,14 @@ class LoginTestCase(TestCase):
         
     def test_login_failure(self):
         
-        user = authenticate(username = 'ngoc', password = '123123123')
+        user = authenticate(username = 'higay', password = '123123123')
         self.assertIsNone(user)
+
+#agents
+class AgentTestCase(TestCase):
+    def setUp(self):
+        self.agent = agents_model.models.Model(name='Viper', country = 'USA')
+    
+    def test_agent_test(self):
+        obj1 = agents_model.models.get(name='Viper')
+        self.assertEqual(obj1.name,"Viper")
