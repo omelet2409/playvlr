76806ef295f2cddf7ab185295b01f335623704fb
omelet2409 <thelong2409@gmail.com>
2023-05-09 19:25:26 +0700

    omelet

--- home/models/home_model.py
+++ home/models/home_model.py
@@ -1,6 +1,7 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 from django.urls import reverse
+from django.utils.text import slugify
 # Create your models here.
 
 class Home(models.Model):
