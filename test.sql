ccb54fe25d83644c98ab3b6c6f3eac6939898884
omelet2409 <thelong2409@gmail.com>
2023-10-12 12:24:29 +0700

    update

---
+++ test.sql
@@ -0,0 +1,8 @@
+SELECT * FROM home_tutorial
+
+INSERT INTO home_tutorial 
+VALUES ('8', 'image/upload/v1682163593/yccbwyhqpjeeldeimo63.jpg', '123');
+
+UPDATE home_tutorial
+SET img1 = 'image/upload/v1682163593/yccbwyhqpjeeldeimo63.jpg', description = '12323'
+WHERE id = 3
